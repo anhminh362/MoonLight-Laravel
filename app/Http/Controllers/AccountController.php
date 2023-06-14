54c5ce94945eb8b2939e99e830e9edadc68ba824
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-06-14 08:11:34 +0700

    login controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AccountController extends Controller
 {
@@ -15,6 +17,41 @@ class AccountController extends Controller
     }
     protected function store(Request $request):void{
         Account::create($request->all());
+    }
+    public function login(Request $request){
+        $login=[
+            'email'=>$request->input('email'),
+            'password'=>$request->input('pw')
+        ];
+        if (Auth::attempt($login)) {
+            $user = Auth::guard('account_guard')->user();
+            if ($user->verify == 1) {
+                $token = $user->createToken('API Token')->plainTextToken;
+                return response()->json([
+                    'message' => 'Login successfully',
+                    'token' => $token,
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'Account not verified',
+                ], 401);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Invalid email or password',
+            ], 401);
+        }
+    }
+    protected function Logout(){
+        $user = Auth::user();
+        $tokenName = 'API Token'; // Replace with the desired token name
+        $user->tokens()->where('name', $tokenName)->delete();
+
+        // Perform any additional logout logic, if necessary
+
+        return response()->json(['message' => 'Logged out successfully']);
+//        Session::forget('user');
+//        Session::forget(cart);
     }
 //    protected function show($id){
 //        $account=Account::find($id);
