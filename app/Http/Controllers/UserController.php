9e213f6fc022505fad8681b38a7981e13b0382ce
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-06-20 10:42:24 +0700

    MC-9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,40 +7,84 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    protected function index(){
-        $user=User::all();
-        return $user;
+    protected function index()
+    {
+        $users = User::all();
+        return response()->json($users, 200);
     }
-    protected function show(string $id){
+
+    protected function show(string $id)
+    {
         $user = User::find($id);
 
-        if (!$user){
+        if (!$user) {
             return response()->json([
-                'message' => 'user not found',
-            ],404);
+                'message' => 'User not found',
+            ], 404);
         }
-        return response()->json($user,200);
+
+        return response()->json($user, 200);
     }
-    protected function block($id){
-        $user=User::find($id);
-        if($user->status===1){
-            $user->status=0;
-            $user->save();
-            return ['block successfully'];
-        }
-        return ['cannot block'];
-    }
-    protected function unblock($id): array
+
+    protected function block($id)
     {
-        $user=User::find($id);
-        if($user->status===0){
-            $user->status=1;
-            $user->save();
-            return ['unblock successfully'];
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
         }
-        return ['cannot unblock'];
+
+        if ($user->status === 1) {
+            $user->status = 0;
+            $user->save();
+            return response()->json(['message' => 'User blocked successfully'], 
+            200);
+        }
+
+        return response()->json(['message' => 'Cannot block user'], 400);
     }
-    protected function store(Request $request){
+
+    protected function unblock($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        if ($user->status === 0) {
+            $user->status = 1;
+            $user->save();
+            return response()->json(['message' => 'User unblocked successfully'], 200);
+        }
+
+        return response()->json(['message' => 'Cannot unblock user'], 400);
+    }
+
+    protected function destroy(string $id)
+{
+    $user = User::find($id);
+
+    if (!$user) {
+        return response()->json([
+            'message' => 'User not found',
+        ], 404);
+    }
+
+    $user->delete();
+
+    return response()->json(['message' => 'User deleted successfully'], 200);
+}
+
+
+    protected function store(Request $request)
+    {
         return User::create($request->all());
     }
 }
+
+
