4efc35555db3a82a001ef5915c61cd00911d5afb
KieuMei2811 <hothikieu28112003@gmail.com>
2023-07-05 16:56:57 +0700

    mc-118

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,64 +9,86 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    protected function like(Request $request){
-        $userId = auth('sanctum')->user()->id;//chuyen token thanh user_id
-        $movie=$request->input('movie_id');
-        if (!$userId) {
-            return response()->json([
-                'message' => 'Please log in',
-            ]);
-        }
-        $like = Like::where('user_id', $userId)->where('movie_id', $movie)->first();
-        if($like){
-            return response()->json([
-                'message' => 'Already liked',
-            ]);
-            
-        }
-        $like = new Like();
-        $like->movie_id = intval($request->input('movie_id'));
-        $like->user_id = intval($request->input('user_id'));
-        $like->save();
+    protected function like(Request $request)
+{
+    $userId = auth('sanctum')->user()->id;
+    $movie = $request->input('movie_id');
+
+    if (!$userId) {
         return response()->json([
-            'message' => 'Like succesful',
-            'like'=> $like,
+            'message' => 'Please log in',
         ]);
-        
-        
     }
 
-    protected function unlike(Request $request){
-        $user = $request->input('user_id');
-        $movie=$request->input('movie_id');
+    $like = Like::where('user_id', $userId)->where('movie_id', $movie)->first();
+    if ($like) {
+        return response()->json([
+            'message' => 'Already liked',
+        ]);
+    }
+
+    $newLike = new Like();
+    $newLike->movie_id = intval($request->input('movie_id'));
+    $newLike->user_id = intval($request->input('user_id'));
+    $newLike->save();
+
+    // Lấy số lượt thích hiện tại của phim
+    $numLikes = Like::where('movie_id', $movie)->count();
+
+    return response()->json([
+        'message' => 'Like successful',
+        'like' => $newLike,
+        'num_likes' => $numLikes,
+    ]);
+}
+
+protected function unlike(Request $request)
+{
+    $user = $request->input('user_id');
+    $movie = $request->input('movie_id');
+
     if (!$user) {
         return response()->json([
             'message' => 'Please log in',
         ]);
     }
+
     $like = Like::where('user_id', $user)->where('movie_id', $movie)->first();
-    if($like){
+    if ($like) {
         $like->delete();
+
+        // Lấy số lượt thích hiện tại của phim
+        $numLikes = Like::where('movie_id', $movie)->count();
+
         return response()->json([
-            'message' => 'Unlike already',
+            'message' => 'Unlike successful',
+            'num_likes' => $numLikes,
+        ]);
+    } else {
+        return response()->json([
+            'message' => 'Did not like yet',
         ]);
     }
-    else return response()->json([
-        'message' => 'Did not like yet',
-    ]);
 }
-    protected function destroy(string $id){
-        $like=Like::find($id);
-        $like->delete();
-    }
-    protected function show(string $id){
-        $num=Like::where('movie_id',$id)
+
+protected function getLikesByMovieId(string $id)
+{
+    $num = Like::where('movie_id', $id)
         ->groupBy('movie_id')
         ->count();
-        return $num;
-    }
-    protected function index(){
-        $like=Like::all();
-        return response()->json([$like],200);
-    }
+
+    return $num;
+}
+
+protected function index()
+{
+    $likes = Like::all();
+
+    return response()->json($likes, 200);
+}
+
+    // protected function index(){
+    //     $like=Like::all();
+    //     return response()->json([$like],200);
+    // }
 }
